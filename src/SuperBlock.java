8e37f6d9c0ca641c2ae37fe2f66a4dacd7405fb2
hurdj3 <jimi8705@gmail.com>
2014-11-23 14:20:14 -0800

    added SuperBlock.java stub

---
+++ src/SuperBlock.java
@@ -0,0 +1,29 @@
+
+public class SuperBlock {
+	private final int defaultInodeBlocks = 64;
+	public int totalBlocks;
+	public int totalInodes;
+	public int freeList;
+	
+	public SuperBlock (int diskSize) {
+		// read the superblock from the disk
+		byte[] superBlock = new byte[Disk.blockSize];
+		SysLib.rawread (0, superBlock);
+		totalBlocks = SysLib.bytes2int(superBlock, 0);
+		totalInodes = SysLib.bytes2int(superBlock, 4);
+		freeList = SysLib.bytes2int(superBlock, 8);
+		
+		if( totalBlocks == diskSize && totalInodes > 0 && freeList >= 2){
+			//disk contents are valid
+			return;
+		} else {
+			// need to format disk
+			totalBlocks = diskSize;
+			format(defaultInodeBlocks);
+		}
+			
+		
+		
+		
+	}
+}
