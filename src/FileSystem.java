9cc86bbaad5ae330b6312fcc5a41d46af95197df
hurdj3 <jimi8705@gmail.com>
2014-12-06 11:05:53 -0800

    Uncommented ctor in fileSystem

--- src/FileSystem.java
+++ src/FileSystem.java
@@ -17,14 +17,14 @@ public class FileSystem {
 		directory = new Directory(superBlock.totalInodes);
 		fileTable = new FileTable(this, directory);
 
-		// FileTableEntry dirEnt = open("/", "r");
-		// int dirSize = fsize(dirEnt);
-		// if(dirSize > 0) {
-		// 	byte[] dirData = new byte[dirSize];
-		// 	read(dirEnt, dirData);
-		// 	directory.bytes2directory(dirdata);
-		// }
-		// close (dirEnt);
+		FileTableEntry dirEnt = open("/", "r");
+		int dirSize = fsize(dirEnt);
+		if(dirSize > 0) {
+		 	byte[] dirData = new byte[dirSize];
+		 	read(dirEnt, dirData);
+		 	directory.bytes2directory(dirdata);
+		}
+		close (dirEnt);
 	}
 
 	public int loadInodeCache(Inode[] cache) {
