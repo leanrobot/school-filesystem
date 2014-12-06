5e5f6d68ca95e53b49f88beb55fef71132d4eee1
Brittany Bentley <bbcbentley@gmail.com>
2014-12-06 14:14:49 -0800

    Commented FileTable

--- src/FileTable.java
+++ src/FileTable.java
@@ -1,3 +1,14 @@
+/******************************************************************************
+* FileTable.java
+* Programmed by: Brittany Bentley, James Hurd, Thomas Petit
+* Class: CSS430 - Operating Systems
+* Quarter: Autumn 2014
+* University of Washington, Bothell
+*  
+*
+******************************************************************************/
+
+
 import java.util.Vector;
 
 public class FileTable {
@@ -9,10 +20,15 @@ public class FileTable {
       public FileTable(FileSystem fs, Directory directory ) { // constructor
          table = new Vector<FileTableEntry>();     // instantiate a file (structure) table
          dir = directory;           // receive a reference to the Director
-         this.fs = fs;
-      }                             // from the file system
+         this.fs = fs;				// from the file system
+      }                             
 
-      // major public methods
+      
+      // allocate a new file table entry for this file name
+      // allocate/retrieve and register the corresponding inode using dir
+      // increment this inode's count
+      // immediately write back this inode to the disk
+      // return a reference to this file table entry
       public synchronized FileTableEntry falloc( String filename, String mode ) {
          short iNumber = dir.namei(filename);
          if(SysLib.isError(iNumber)) {
@@ -26,27 +42,23 @@ public class FileTable {
          }
          table.add(fte);
          return fte;
-         
-         // allocate a new file (structure) table entry for this file name
-         // allocate/retrieve and register the corresponding inode using dir
-         // increment this inode's count
-         // immediately write back this inode to the disk
-         // return a reference to this file (structure) table entry
       }
 
+      
+      // receive a file table entry reference
+      // save the corresponding inode to the disk
+      // free this file table entry.
+      // return true if this file table entry found in my table
       public synchronized boolean ffree( FileTableEntry e ) {
          synchronized(e.inode) {
             e.inode.count--;
             e.inode.toDisk(e.iNumber);
          }
          return table.remove(e);
-
-         // receive a file table entry reference
-         // save the corresponding inode to the disk
-         // free this file table entry.
-         // return true if this file table entry found in my table
       }
 
+      //empties the file table of all entries
+      //returns whether or not the table is empty
       public synchronized boolean fempty( ) {
          for(FileTableEntry fte : table) {
             ffree(fte);
