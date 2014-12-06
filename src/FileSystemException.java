fc2fadcade6940ae2a17819b0b6bf0f851715d5c
hurdj3 <jimi8705@gmail.com>
2014-12-06 13:58:22 -0800

    Comments for FileSystemException

--- src/FileSystemException.java
+++ src/FileSystemException.java
@@ -1,3 +1,14 @@
+/******************************************************************************
+* FileSystemException.java
+* Programmed by: Brittany Bentley, James Hurd, Thomas Petit
+* Class: CSS430 - Operating Systems
+* Quarter: Autumn 2014
+* University of Washington, Bothell
+*  
+*
+******************************************************************************/
+
+// An exception class for sloppy developers.
 public class FileSystemException extends RuntimeException {
 	public FileSystemException(String message, Throwable cause) {
 		super(message, cause);
